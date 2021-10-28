ea1f419ca27caa2f5210c97824765c38c1d99ed5
Aung Yee <lyndon.lso@gmail.com>
2021-10-28 15:20:26 +0100

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Aung Yee on 28/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
